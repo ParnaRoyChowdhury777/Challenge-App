8c11dfa9d98977a9735de053901acf7c8f648459
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2025-04-05 18:30:51 +0530

    Challenge App done

---
+++ src/test/java/com/parna/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parna.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
